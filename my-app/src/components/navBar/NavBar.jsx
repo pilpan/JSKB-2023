6fc32a28a42e8ae87b531ecc14310cd512573345
plip <plip2010195@yandex.ru>
2023-04-24 12:55:59 +0300

    add new NavBar

--- my-app/src/components/navBar/NavBar.jsx
+++ my-app/src/components/navBar/NavBar.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+        <ol>
+            <li>
+                Home
+            </li>
+            <li>
+                Players
+            </li>
+            <li>
+                Teams
+            </li>
+            <li>
+                Stats
+            </li>
+        </ol>
+    </div>
   )
 }
